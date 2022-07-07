1161667a4bec24c615b23a0f9dd8cc1c588cc726
Riley <rileyc.gamer@gmail.com>
2022-07-07 13:34:26 -0400

    Initial Commit

---
+++ Mind/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mind
+//
+//  Created by RileyCant on 2022-07-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mind/MindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindApp.swift
+//  Mind
+//
+//  Created by RileyCant on 2022-07-07.
+//
+
+import SwiftUI
+
+@main
+struct MindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
